3deba887f4c0d5fcb333d38e18fe1eb11295d2de
Leza Lutonda (lemol) <lemol-c@hotmail.com>
2020-09-04 13:09:31 +0100

    move assets to assets

--- slides/index.js
+++ slides/index.js
@@ -1,7 +1,7 @@
 import "reveal.js/dist/reveal.css";
 import "reveal.js/plugin/highlight/monokai.css";
-import "./black.css";
-import "./style.css";
+import "./assets/black.css";
+import "./assets/style.css";
 
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
