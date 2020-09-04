22d0bbb26e38ce3a24c4d3503a29a7d3c1230ed1
Leza Lutonda (lemol) <lemol-c@hotmail.com>
2020-09-04 03:57:51 +0100

    finishing slides

--- slides/index.js
+++ slides/index.js
@@ -1,11 +1,14 @@
 import "reveal.js/dist/reveal.css";
+import "reveal.js/plugin/highlight/monokai.css";
 import "./black.css";
+import "./style.css";
 
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
 
 const deck = new Reveal({
-  plugins: [Markdown],
+  plugins: [Markdown, Highlight],
 });
 
 deck.initialize();
